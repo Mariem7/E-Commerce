ad7ef27483a9ec9f45fa51fca1d334e7768f4e5f
Maryem Neili <mariemneili7@gmail.com>
2021-12-02 23:32:11 +0100

    commit 5

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -1,78 +1,43 @@
+<!--Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm bg-white rounded">
+  <a class="navbar-brand" href="#">
+    <img class="logo" src='images/logo.png' alt='not found'/>
+  </a>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo lang('HOME_ADMIN')?></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#"><?php echo lang('CATEGORIES')?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo lang('ITEMES')?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo lang('MEMBERS')?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo lang('STATICTICS')?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo lang('LOGS')?></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+        Account
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <div class="dropdown-menu shadow mb-2" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="members.php?do=Edit&userid=<?php echo $_SESSION['ID']?>">Edit Profile</a>
+          <a class="dropdown-item" href="#">Setting</a>
+          <a class="dropdown-item" href="logout.php">Logout</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
   </div>
 </nav>
-
-
-
-
-<!--
-<nav class="navbar navbar-inverse navbar-static-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-nav" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><?php echo lang('HOME_ADMIN')?></a>
-    </div>
-
-    <div class="collapse navbar-collapse" id="app-nav">
-      <ul class="nav navbar-nav">
-        <li><a href="#"><?php echo lang('CATEGORIES')?></a></li>
-        <li><a href="#"><?php echo lang('ITEMES')?></a></li>
-        <li><a href="#"><?php echo lang('MEMBERS')?></a></li>
-        <li><a href="#"><?php echo lang('STATICTICS')?></a></li>
-        <li><a href="#"><?php echo lang('LOGS')?></a></li>
-          </ul>
-        </li>
-      </ul>
-      
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Souha <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="members.php?do=Edit&userid=<?php echo $_SESSION['ID']?>">Edit Profile</a></li>
-            <li><a href="#">Setting</a></li>
-            <li><a href="logout.php">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
--->
